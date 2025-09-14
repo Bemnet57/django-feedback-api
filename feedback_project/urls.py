f847f89700a0407902f204cb8a627809e69ccc03
Bemnet57 <bemnetasseged57@gmail.com>
2025-09-14 22:01:12 +0300

    fixed urls, made swagger root

--- feedback_project/urls.py
+++ feedback_project/urls.py
@@ -1,41 +1,64 @@
-"""
-URL configuration for feedback_project project.
+# """
+# URL configuration for feedback_project project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# The `urlpatterns` list routes URLs to views. For more information please see:
+#     https://docs.djangoproject.com/en/5.2/topics/http/urls/
+# Examples:
+# Function views
+#     1. Add an import:  from my_app import views
+#     2. Add a URL to urlpatterns:  path('', views.home, name='home')
+# Class-based views
+#     1. Add an import:  from other_app.views import Home
+#     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+# Including another URLconf
+#     1. Import the include() function: from django.urls import include, path
+#     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+# """
+# # from django.contrib import admin
+# # from django.urls import path
+
+# # urlpatterns = [
+# #     path('admin/', admin.site.urls),
+# # ]
+# # from django.contrib import admin
+# # from django.urls import path, include
+# # from django.shortcuts import redirect
+
+# # def redirect_to_api(request):
+# #     return redirect("/api/")
+
+# # urlpatterns = [
+# #     path('admin/', admin.site.urls),
+# #     path('api/', include('feedback_app.urls')),
+# #     path("", redirect_to_api),
+# #]
+
+
 # from django.contrib import admin
-# from django.urls import path
+# from django.urls import path, re_path, include 
+# from rest_framework import permissions
+# from drf_yasg.views import get_schema_view
+# from drf_yasg import openapi
 
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-# ]
-# from django.contrib import admin
-# from django.urls import path, include
-# from django.shortcuts import redirect
-
-# def redirect_to_api(request):
-#     return redirect("/api/")
+# schema_view = get_schema_view(
+#    openapi.Info(
+#       title="Feedback API",
+#       default_version='v1',
+#       description="Public API for feedback collection",
+#    ),
+#    public=True,
+#    permission_classes=(permissions.AllowAny,),
+# )
 
 # urlpatterns = [
 #     path('admin/', admin.site.urls),
 #     path('api/', include('feedback_app.urls')),
-#     path("", redirect_to_api),
-#]
-
+#     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+# ]
 
 from django.contrib import admin
-from django.urls import path, re_path, include 
+from django.urls import path, re_path, include
+from django.shortcuts import redirect
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -50,8 +73,12 @@ schema_view = get_schema_view(
    permission_classes=(permissions.AllowAny,),
 )
 
+def redirect_to_swagger(request):
+    return redirect("/swagger/")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('feedback_app.urls')),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path("", redirect_to_swagger),  # 👈 root redirects to Swagger
 ]
